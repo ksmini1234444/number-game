891947ff95314b085a45bd9455f1288627030677
kimmin <ksmini1234444@gmail.com>
2024-06-30 21:41:40 +0900

    second

--- main.js
+++ main.js
@@ -1,40 +1,86 @@
-// 1~100 사이의 번호 중 랜덤하게 정답 번호가 지정되도록 함  
-// 유저가 번호를 입력 go 라는 버튼 누름 
-// 만약 유저가 정답을 맞추면, 맞췄습니다 
-// 정답 < 유저번호 down
-// 정답 > 유저번호 up
-// reset 버튼을 누르면 게임 리셋
-// 5번의 기회를 다 쓰면 게임으 끝난다. 버튼이 disable
+// 1~100 사이의 번호 중 랜덤하게 정답 번호가 지정되도록 함k  
+// 유저가 번호를 입력 go 라는 버튼 누름 k
+// 만약 유저가 정답을 맞추면, 맞췄습니다 k
+// 정답 < 유저번호 downk
+// 정답 > 유저번호 upk
+// reset 버튼을 누르면 게임 리셋k
+
+// 5번의 기회를 다 쓰면 게임이 끝난다. 버튼이 disable
 // 유저가 1~100 범위 밖의 숫자를 입력하면 알려주고 기회를 깍지 않는다 
 // 유저가 이미 입력한 숫자를 또 입력하면 알려주고 기회를 깍지 않는다. 
 
 //컴퓨터가 랜덤으로 뽑은 번호를 저장해둘 변수 만듦 
 let randomNum = 0 ;
-let playButton = document.getElementById("play-button") ;
-let userInputNum = document.getElementById("user-input-num") ;
-let resultShow = document.getElementById("result-out") ;
+let buttonGo = document.getElementById("When-click-go");
+let inputNum = document.getElementById("input-num");
+let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5 ;
+let gameEnd = false ;
+let history = [] ;
+let chanceArea = document.getElementById("chance-area") ;
 
 //컴퓨터가 랜덤 번호를 뽑는 함수 만듦 
-function pickNum () {
+function comPickNum () {
     randomNum = Math.floor(Math.random()*100)+1;
     console.log("정답",randomNum);
 }
 
-pickNum () ;
+comPickNum () ;
 
 //유저가 go라는 버튼을 클릭 했을때 입력한 숫자를 js로 가져오는 이벤트를 넣어줘야함 
 //유저가 go라는 버튼을 눌렀을때 입력한 숫자를 가져오는 함수?
-playButton.addEventListener("click",play) ;
+//콘솔창에 나왔던 결과값(up down 등)을 웹사이트로 옮겨오기?
+buttonGo.addEventListener("click",judge);
+resetButton.addEventListener("click",resetOption);
+inputNum.addEventListener("focus",function(){inputNum.value=""});
 
-function play (){
-    let userInputNumValue = userInputNum.value ;
+function judge () {
+    let userInputNum = inputNum.value ;
 
-    if (userInputNumValue>randomNum) { 
-        resultShow.textContent = "down!!" ;
-    } else if (userInputNumValue<randomNum) {
-        resultShow.textContent = "Up!!!" ;
-    } else {resultShow.textContent="정답입니다^^"}
- 
+    if (userInputNum<1 || userInputNum>100) {
+        resultArea.textContent ="1~100까지의 숫자를 입력해주세요!";
+        return;
+    }
+
+    if (history.includes(userInputNum)) {
+        resultArea.textContent ="중복된 숫자를 입력했습니다. 다시 입력해주세요";
+        return;
+    }
+
+    chances -- ;
+    chanceArea.textContent = `남은 기회는? ${chances}번` ;
+    console.log("남은 기회:", chances) ;
+
+    if (userInputNum>randomNum) {
+        resultArea.textContent ="Down!";
+    } else if (userInputNum<randomNum) {
+        resultArea.textContent ="Up!";
+    } else {resultArea.textContent ="정답입니다!";
+        gameEnd = true ;
+    }
+
+    history.push(userInputNum) ;
+
+    if (chances<1) {
+        gameEnd = true ;
+    }
+
+    if (gameEnd == true) {
+        buttonGo.disabled = true ;
+    }
+
 }
 
-//콘솔창에 나왔던 결과값(up down 등)을 웹사이트로 옮겨오기?
+
+//reset 버튼을 누르면 게임 리셋
+//user의 input 창에 아무것도 안뜨고 
+//새로운 정답번호가 부여됨 
+function resetOption() {
+    inputNum.value = "" ;
+    comPickNum () ;
+     resultArea.textContent ="게임을 다시 시작합니다";
+     buttonGo.disabled = false ; 
+     chances = 5 ;
+     chanceArea.textContent = `남은 기회는? ${chances}번` ;
+}
